691480917d230826c3fd736707c309dff70d0413
LuanRethink <124921888+LuanRethink@users.noreply.github.com>
2023-05-26 14:40:44 -0300

    can not use class, changed to className

--- src/components/loader/index.jsx
+++ src/components/loader/index.jsx
@@ -2,7 +2,7 @@ import "./styles.css";
 const Loader = () => {
   return (
     <div className="loaderDiv">
-      <span class="loader"></span>
+      <span className="loader"></span>
     </div>
   );
 };
