ca85e6ae9b3cb68687b42a0a0a05fffe5057724f
LuanRethink <124921888+LuanRethink@users.noreply.github.com>
2023-05-27 02:31:07 -0300

    dropdown

---
+++ src/view/products/dropdown.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+function Dropdown({ value, children, dropdownValue, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const toggleDropDown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const categoriesInput = dropdownValue?.map((el) => {
+    return (
+      <li key={el} onClick={() => handleOptionSelect(el)}>
+        {`${el}`}
+      </li>
+    );
+  });
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className="dropdownContainer" onClick={toggleDropDown}>
+      <button className={`dropdown-toggle ${isOpen ? "active" : ""}`}>
+        <div className="selectedOption">
+          {selectedOption ? selectedOption : "Selecione uma opção:"}
+        </div>
+        <div className="arrowContainer">
+          <span className={`arrow ${isOpen ? "active" : ""}`}></span>
+        </div>
+      </button>
+      <ul className={`menu ${isOpen ? "active" : ""}`}>
+        {categoriesInput}
+
+        <li onClick={() => handleOptionSelect("Valor2")}>Valor2</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Dropdown;
