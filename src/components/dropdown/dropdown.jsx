27f320f5ac2f33eab79f63dd6d59bb3d2c51df26
LuanRethink <124921888+LuanRethink@users.noreply.github.com>
2023-05-29 23:15:56 -0300

    dropdown repository changed

---
+++ src/components/dropdown/dropdown.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function Dropdown({ children, dropdownValue, onChange }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const toggleDropDown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const categoriesInput = dropdownValue?.map((el) => {
+    return (
+      <li key={el} onClick={() => handleOptionSelect(el)}>
+        {`${el}`}
+      </li>
+    );
+  });
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    setIsOpen(false);
+    onChange(option);
+  };
+
+  return (
+    <div className="dropdownContainer" onClick={toggleDropDown}>
+      <button className={`dropdown-toggle ${isOpen ? "active" : ""}`}>
+        <div className="selectedOption">
+          {selectedOption ? selectedOption : "Selecione uma opção:"}
+        </div>
+        <div className="arrowContainer">
+          <span className={`arrow ${isOpen ? "active" : ""}`}></span>
+        </div>
+      </button>
+      <ul className={`menu ${isOpen ? "active" : ""}`}>{categoriesInput}</ul>
+    </div>
+  );
+}
+
+export default Dropdown;
