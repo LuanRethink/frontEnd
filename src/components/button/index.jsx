e2948ad61137e08d69990fbf2a8842801f368a3a
luan <luan.barreiros@rethink.dev>
2023-06-04 21:28:31 -0300

    added a disable option to button

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,12 +1,21 @@
 import "./styles.css";
 
-const Button = ({ title, type, onclick, visible, background, children }) => {
+const Button = ({
+  title,
+  type,
+  onclick,
+  visible,
+  background,
+  children,
+  disabled,
+}) => {
   return (
     <>
       <button
         type={type}
         className={`button ${background} ${visible ? "visible" : "hidden"}`}
         onClick={onclick}
+        disabled={disabled}
       >
         {title}
         {children}
